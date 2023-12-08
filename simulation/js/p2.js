8cc224dfc4255e76534542fda6b2161949418b0a
vleadadmin <admin@vlabs.ac.in>
2023-12-08 06:19:46 +0000

    https://virtual-labs.github.io/virtual-labs/exp-characteristic-parameters-iitr click on the link to test your code.

---
+++ simulation/js/p2.js
@@ -0,0 +1,25 @@
+Ui.P2 = function() {
+
+};
+
+Ui.P2.prototype = Object.create(Ui.prototype);
+
+Ui.P2.prototype.createElement = function() {
+  "use strict";
+  Ui.prototype.createElement.apply(this, arguments);
+  this.addComponent(new Ui.Pointer({
+    type: 'Rect',
+    pointerWidth: 3,
+    pointerHeight: this.width / 5,
+    offset: this.width / 2 - this.width / 3.3 - this.width / 10
+  }));
+
+  this.addComponent(new Ui.Scale(this.merge(this.options, {
+    drawScale: false,
+    drawDial: true,
+    radius: this.width/2.6})));
+
+  var circle = new Ui.El.Circle(this.width / 4.5, this.width / 2, this.height / 2);
+  this.el.node.appendChild(circle.node);
+  this.el.node.setAttribute("class", "p2");
+};
